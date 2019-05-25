f456db5e3c2bb36008a68fbc180daf28056492ad
Martin Broerse <broerse@martinic.com>
2019-05-25 10:38:39 +0200

    Try it in ember 2.18

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,22 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
   color: 'red',
+
+  rects: computed(function() {
+    let rects = [];
+    for (let x = 0; x < 80; x++) {
+      for (let y = 0; y < 60; y++) {
+        let fill = 'red';
+        if (x === 0 || x === 79 || y === 0 || y === 59) {
+          fill = 'blue';
+        }
+        rects.push({x: x, y: y, fill: fill});
+      }
+    }
+    return rects;
+  }),
 
   actions: {
     paint: function() {
